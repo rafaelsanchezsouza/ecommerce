5e71c5b64cdd1e22addc016a6ef1a7f3b6624f60
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-01-05 20:49:48 -0300

    create_images orders

---
+++ src/database/migrations/1609764580876-create_images.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createImages1609764580876 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'images',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'path',
+            type: 'varchar',
+          },
+          {
+            name: 'product_id',
+            type: 'integer',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'orderImage',
+            columnNames: ['product_id'],
+            referencedTableName: 'order',
+            referencedColumnNames: ['id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('images');
+  }
+}
