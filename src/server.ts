51be5d5732f19f2efb9946717934deaca80aeec1
Rafael Sanchez Souza <rafaelsanchezsouza@gmail.com>
2021-01-14 22:56:28 -0300

    cors installed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import 'express-async-errors';
-
+import cors from 'cors';
 import './database/connection';
 
 import routes from './routes';
@@ -10,6 +10,7 @@ import errorHandler from './errors/handler';
 const app = express();
 app.use(express.json()); //para que express consiga ler json
 
+app.use(cors);
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
